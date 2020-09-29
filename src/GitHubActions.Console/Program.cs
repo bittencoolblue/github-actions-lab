7da6aa338661c9a2a91dfb9777d4c01dd8950c7c
Andre Bittencourt <a.bittencourt@coolblue.nl>
2020-09-29 23:50:00 +0200

    Add projects

---
+++ src/GitHubActions.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubActions.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
